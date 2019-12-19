5a74bdc206a7f7af5262d247d11ccd4f22cd6058
johannesriedmueller <riejoc16@htlkaindorf.at>
2019-12-19 11:45:26 +0100

    Added config file and loading of config file

--- udp-client.py
+++ udp-client.py
@@ -1,8 +1,16 @@
 import socket
+import yaml
+
 
 class UDPClient:
     def __init__(self):
-        print("init");
+        print("init")
+        with open("config.yaml", 'r') as yamlfile:
+            cfg = yaml.safe_load(yamlfile)
+
+        self.server_ip = cfg['server-ip']
+        self.server_port = cfg['server-port']
+
 
 if __name__ == "__main__":
     udp_client = UDPClient()
